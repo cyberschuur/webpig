96a76f93bc45a14810dc283008b6ede38eb2eebd
Zohar Cochavi <zohar.cochavi@vooruit.nl>
2025-03-23 18:56:10 +0000

    Add 'dnsdumpster' source

--- webpig/source/__init__.py
+++ webpig/source/__init__.py
@@ -1,15 +1,19 @@
 from enum import Enum
 
+from webpig.source import dnsdumpster
+
 from .import crtsh
 from .base import Source
 
 
 class ValidSource(str, Enum):
     CRTSH = crtsh.CrtSh.name()
+    DNSDUMPSTER = dnsdumpster.DnsDumpster.name()
 
 
 all_sources: dict[ValidSource, type[Source]] = {
     ValidSource.CRTSH: crtsh.CrtSh,
+    ValidSource.DNSDUMPSTER: dnsdumpster.DnsDumpster,
 }
 
 __all__ = ["ValidSource", "all_sources"]
---
+++ webpig/source/dnsdumpster.py
@@ -0,0 +1,60 @@
+import os
+import logging
+
+from typing import Generator, Optional
+from rich.prompt import Prompt
+
+import requests
+
+from .. import console, progress
+from .base import Source
+
+
+logger = logging.getLogger()
+
+
+class DnsDumpster(Source):
+    @staticmethod
+    def _get_api_key() -> Optional[str]:
+        # api_key = os.getenv("DNSDUMPSTER_API_KEY")
+        api_key = None
+
+        if not api_key:
+            progress.stop()
+            api_key = Prompt.ask(
+                prompt="Please provide your DNSDUMPSTER_API_KEY",
+                password=True,
+                default=None,
+                console=console
+            )
+            progress.start()
+
+        return api_key
+
+    @staticmethod
+    def name() -> str:
+        return "dnsdumpster"
+
+    @staticmethod
+    def find(domain: str) -> Generator[str, None, None]:
+        api_key = DnsDumpster._get_api_key()
+
+        if not api_key:
+            logger.error(
+                "DNSDUMPSTER_API_KEY not set and prompt not provided. Skipping dnsdumpster source.")
+            return
+
+        try:
+            url = f"https://api.dnsdumpster.com/domain/{domain}"
+            response = requests.get(url, headers={"X-Api-Key": api_key})
+            response.raise_for_status()
+        except requests.RequestException as ce:
+            logger.error(
+                f"Failed to fetch dnsdumpster data for {domain}: {ce}")
+            return
+
+        # for now, just return the domain names from the A records
+        for record in response.json()["a"]:
+            yield record["host"]
+
+        yield url
